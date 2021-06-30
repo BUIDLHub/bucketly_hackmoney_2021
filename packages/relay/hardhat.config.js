31d61c4f0ce2bc71fc4a0e60cfa2743eb66752ab
raphaelpg <raphael_pinto@outlook.com>
2021-06-30 15:50:33 +0100

    set hardhat artificats path inside relay src/contracts folder

--- packages/relay/hardhat.config.js
+++ packages/relay/hardhat.config.js
@@ -5,4 +5,10 @@ require("@nomiclabs/hardhat-waffle");
  */
 module.exports = {
   solidity: "0.8.4",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./src/contracts"
+  },
 };
