a185f88940db25eb4505baed52bcdf6c9c303863
Adebayo <daramola.adebayo@yahoo.com>
2021-07-01 14:09:05 +0100

    feature(components): add address and identicon component - add network status component - add balance component

---
+++ packages/frontend/src/components/Address.jsx
@@ -0,0 +1,21 @@
+import { useWeb3React } from '@web3-react/core';
+import Icon from 'react-identicons';
+
+const Address = () => {
+  const { account } = useWeb3React()
+
+  return (
+    <span className="d-inline-flex align-items-center border px-2 py-1">
+      {account && <span className="pr-2"><Icon string={account} size={25} /></span>}
+      <span>
+        {account === null
+          ? '-'
+          : account
+            ? `${account.substring(0, 6)}...${account.substring(account.length - 4)}`
+            : ''}
+      </span>
+    </span>
+  )
+}
+
+export default Address;
